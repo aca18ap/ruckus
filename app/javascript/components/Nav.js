03e79be8b7243c979792e4cb34482753faa3b64e
Alberto <apia1@sheffield.ac.uk>
2022-10-23 18:39:51 +0100

    React added and navbar component created

---
+++ app/javascript/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <div className="navbar">
+        
+    <h1 className="text-black">RUCKUS PEAK</h1>
+    </div>
+  );
+}
+
+export default Nav
